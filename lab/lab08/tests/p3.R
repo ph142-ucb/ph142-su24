b3ed8fb2e6883e097da44d7d5012d2e5b1d9a74d
Paula Marquez <paulamarquez@berkeley.edu>
2024-07-28 03:15:13 -0700

    test case fix

--- lab/lab08/tests/p3.R
+++ lab/lab08/tests/p3.R
@@ -6,9 +6,10 @@ test = list(
       name = NA,
       points = 1.0,
       code = {
-        test_that("p3a", {
-          expect_true(dim(deeks_wide) == c(71, 3))
-          print("Checking: deeks_wide is in wide format")
+        test_that("p4a", {
+          expect_true(nrow(deeks_wide) == 71, 
+                      ncol(deeks_wide) == 4)
+          print("Checking: Added another column to the deeks_wide dataset")
         })
       }
     )
