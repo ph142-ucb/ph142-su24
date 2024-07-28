2faae783faa17247261cf8a17a0b70c4e57d6bc2
Paula Marquez <paulamarquez@berkeley.edu>
2024-07-28 03:18:41 -0700

    lab8 test case

--- lab/lab08/tests/p3.R
+++ lab/lab08/tests/p3.R
@@ -6,7 +6,7 @@ test = list(
       name = NA,
       points = 1.0,
       code = {
-        test_that("p4a", {
+        test_that("p3a", {
           expect_true(nrow(deeks_wide) == 71, 
                       ncol(deeks_wide) == 4)
           print("Checking: Added another column to the deeks_wide dataset")
